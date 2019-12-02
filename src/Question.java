30fb59f8fd4669d516a4a3cd2141aee1cdd7e8ae
arotto8719 <arotto87@gmail.com>
2019-12-02 11:10:13 -0600

    quiz

---
+++ src/Question.java
@@ -0,0 +1,25 @@
+public class Question {
+
+public abstract class Question() {
+        public abstract String prompt;
+        public abstract Boolean answer;
+//        private String prompt;
+//        private String answer;
+//        int Double result;
+
+//        displayPrompt(): void;
+//    {abstract}evaluateAnswer(String): boolean;
+//    {abstract}displayChoices(): void;
+    }
+    class MultipleChoice {
+        int choices String[];
+
+    }
+    class CheckBox {
+        int choices String[];
+    }
+    class TrueFalse {
+
+    }
+
+}
---
+++ src/QuizRunner.java
@@ -0,0 +1,5 @@
+public class QuizRunner {
+    public class Main {
+
+    }
+}
